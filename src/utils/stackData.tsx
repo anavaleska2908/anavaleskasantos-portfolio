3159306bdfcb1275c01aef9b805f7fb4d696b39b
Ana Valeska Santos de Souza <avaleska2908@hotmail.com>
2022-09-08 20:42:25 -0300

    feat: creating the files inside the utils folder with data needed to access the github api, contact information and learned technologies.

---
+++ src/utils/stackData.tsx
@@ -0,0 +1,112 @@
+import {
+	FaHtml5,
+	FaCss3Alt,
+	FaReact,
+	FaNodeJs,
+	FaDocker,
+	FaPython,
+	FaGithub,
+	FaGitSquare,
+} from "react-icons/fa";
+import {
+	SiStyledcomponents,
+	SiExpress,
+	SiPostgresql,
+	SiMongodb,
+	SiJavascript,
+	SiDjango,
+	SiTypescript,
+	SiRedux,
+	SiJest,
+	SiCypress,
+	SiAdonisjs,
+} from "react-icons/si";
+import { BsFillKanbanFill } from "react-icons/bs";
+import { DiScrum } from "react-icons/di";
+
+export const stackData = [
+	{
+		title: "HTML",
+		img: FaHtml5,
+	},
+	{
+		title: "CSS3",
+		img: FaCss3Alt,
+	},
+	{
+		title: "Javascript",
+		img: SiJavascript,
+	},
+	{
+		title: "ReactJS",
+		img: FaReact,
+	},
+	{
+		title: "NodeJS",
+		img: FaNodeJs,
+	},
+	{
+		title: "Express",
+		img: SiExpress,
+	},
+	{
+		title: "PostgreSQL",
+		img: SiPostgresql,
+	},
+	{
+		title: "Styled-componentes",
+		img: SiStyledcomponents,
+	},
+	{
+		title: "Docker",
+		img: FaDocker,
+	},
+	{
+		title: "MongoDB",
+		img: SiMongodb,
+	},
+	{
+		title: "Python",
+		img: FaPython,
+	},
+	{
+		title: "Django",
+		img: SiDjango,
+	},
+	{
+		title: "Typescript",
+		img: SiTypescript,
+	},
+	{
+		title: "Redux",
+		img: SiRedux,
+	},
+	{
+		title: "Github",
+		img: FaGithub,
+	},
+	{
+		title: "Git",
+		img: FaGitSquare,
+	},
+	{
+		title: "Jest",
+		img: SiJest,
+	},
+	{
+		title: "Cypress",
+		img: SiCypress,
+	},
+	{
+		title: "AdonisJS",
+		img: SiAdonisjs,
+	},
+	{
+		title: "Kanban",
+		img: BsFillKanbanFill,
+	},
+	{
+		title: "Scrum",
+		img: DiScrum,
+	},
+];
